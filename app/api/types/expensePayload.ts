9e8e00d8c1e35190450f8af5a5d1e72b7fcfaeb7
Gustavo Paes <gustavo910@gmail.com>
2025-11-19 12:34:23 -0300

    first commit

--- app/api/types/expensePayload.ts
+++
@@ -1,6 +0,0 @@
-export interface ExpensePayload {
-  description: string;
-  value: number;
-  due_date: string; // formato "YYYY-MM-DD"
-  periodicity: number;
-}
--- app/api/types/sumary.ts
+++
@@ -1,6 +0,0 @@
-
-export interface Sumary {
-  totalDue: number;
-  amountPaid: number;
-  remaining: number;
-}
